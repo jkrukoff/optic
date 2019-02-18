670317932b584939e4819fc249a36d8eb693b3bf
John Krukoff <john@krukoff.us>
2019-02-18 13:32:03 -0700

    Complete test coverage.
    This adds at least some test coverage to all exported functions.

--- test/test_optic.erl
+++ test/test_optic.erl
@@ -18,12 +18,58 @@ new_test_() ->
     [?_assert(is_tuple(optic:new(Fold))),
      ?_assert(is_tuple(optic:new(Fold, Fold)))].
 
+from_test() ->
+    ?assert(is_tuple(optic:from([id(), id()]))).
+
+'%extend_test_'() ->
+    New = fun (_Data, _Template) -> undefined end,
+    [?_assert(is_tuple(optic:'%extend'(id(), #{}, New))),
+     ?_assert(is_tuple(optic:'%extend'(id(), #{strict=>false}, New))),
+     ?_assert(is_tuple(optic:'%extend'(id(), #{strict=>true}, New))),
+     ?_assert(is_tuple(optic:'%extend'(id(), #{create=>undefined}, New))),
+     ?_assertEqual(optic:'%extend'(id(), #{}, New),
+                   optic:'%extend'(id(), #{strict=>false}, New)),
+     ?_assertEqual(optic:'%extend'(id(), [], New),
+                   optic:'%extend'(id(), #{strict=>false}, New)),
+     ?_assertEqual(optic:'%extend'(id(), [strict], New),
+                   optic:'%extend'(id(), #{strict=>true}, New)),
+     ?_assertEqual(optic:'%extend'(id(), [create], New),
+                   optic:'%extend'(id(), #{create=>true}, New))].
+
+fold_test() ->
+    ?assertEqual({ok, 2},
+                 optic:fold([id()],
+                            atom,
+                            fun (atom, Acc) -> Acc + 1 end,
+                            1)).
+
+get_test() ->
+    ?assertEqual({ok, [atom]},
+                 optic:get([id()], atom)).
+
+mapfold_test() ->
+    ?assertEqual({ok, {modified, 2}},
+                 optic:mapfold([id()],
+                               atom,
+                               fun (atom, Acc) -> {modified, Acc + 1} end,
+                               1)).
+
+map_test() ->
+    ?assertEqual({ok, modified},
+                 optic:map([id()],
+                           atom,
+                           fun (atom) -> modified end)).
+
+put_test() ->
+    ?assertEqual({ok, modified},
+                 optic:put([id()], atom, modified)).
+
 %%%===================================================================
 %%% Internal Functions
 %%%===================================================================
 
 id() ->
-    Fold = fun (Fun, Acc, Data) ->
+    MapFold = fun (Fun, Acc, Data) ->
         {ok, Fun(Data, Acc)}
     end,
-    optic:new(Fold, Fold).
+    optic:new(MapFold).
--- test/test_optic_lists.erl
+++ test/test_optic_lists.erl
@@ -14,12 +14,16 @@
 all_get_test_() ->
     [?_assertEqual({ok, [1, 2, 3]},
                    optic:get([optic_lists:all([strict])], [1, 2, 3])),
+     ?_assertEqual({ok, []},
+                   optic:get([optic_lists:all()], atom)),
      ?_assertEqual({error, undefined},
                    optic:get([optic_lists:all([strict])], atom))].
 
 all_put_test_() ->
     [?_assertEqual({ok, [4, 4, 4]},
                    optic:put([optic_lists:all([strict])], [1, 2, 3], 4)),
+     ?_assertEqual({ok, atom},
+                   optic:put([optic_lists:all()], atom, 4)),
      ?_assertEqual({error, undefined},
                    optic:put([optic_lists:all([strict])], atom, 4))].
 
